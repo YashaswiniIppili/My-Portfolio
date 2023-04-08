cb336c74dfe562c46194a4764d2162b24ae22736
Yashaswini Ippili <92773216+YashaswiniIppili@users.noreply.github.com>
2023-04-08 11:15:10 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,20 +8,34 @@ const Portfolio = () => {
       <h2>Recent Work</h2>
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-            <h3>PESU IO</h3>
+            <h3>PESU IO'22</h3>
             <small>The course I enrolled myself into was Explainable AI. I've learnt about concepts like LIME & SHAP. Curated the final project where we created a model that would categorize a YT video's comments into spam or not spam.</small>
         </article>
         <article className='portfolio__item'>
-            <h3>The Hands of Hope Foundation</h3>
-           <small>Was part of the AIKYA hackathon, where we helped the NGO by making a website that would connect to people suffering from trauma etc.</small>
+            <h3>The Hands of Hope Foundation'22</h3>
+           <small>Made it to Top 8 teams out of 30+ teams held by AIKYA. We helped the NGO by making a website that would connect to people suffering from trauma etc.</small>
         </article>
         <article className='portfolio__item'>
-            <h3>KodiKon</h3>
-            <small>Held by the Embrione Club, Worked on helping the government with promoting Online Education. Created a portal that contained videos, accepting donations, timetables etc.</small>
+            <h3>KodiKon'22</h3>
+            <small>Made it to Top 30 out of 210+ teams held by the Embrione Club, Worked on helping the government with promoting Online Education. Created a portal that contained videos, accepting donations, timetables etc.</small>
         </article>
         <article className='portfolio__item'>
             <h3>Blockchain Hackathon</h3>
             <small>Took part in the Blockchain Hackathon held by the ISFCR club. Worked on implementing a blockchain connection.</small>
+        </article>
+            <article className='portfolio__item'>
+            <h3>Dotslash'23 4.0</h3>
+            <small>Implemented VAK Categorization. Provides the user with the required type of resources (Aural, Visual or Kinesthetic).</small>
+        </article>
+        </article>
+            <article className='portfolio__item'>
+            <h3>Hack Attack'23</h3>
+            <small>Held by the ISFCR club of PES. Implemented (both C++ and Python) a working model of Encryption & Decryption of messages. Using AES algorithm.</small>
+        </article>
+        </article>
+            <article className='portfolio__item'>
+            <h3>Terrathon'23 Hackathon</h3>
+            <small>Held by CMS & Alcoding Club. Dealt with Sustainable Development. Implemented a website that would help local producers sell their local produce.</small>
         </article>
       </div>
       <br/>
@@ -30,13 +44,13 @@ const Portfolio = () => {
       <div className='container portfolio__container'>
       <article className='portfolio__item'>
             <h3>AIKYA</h3>
-            Web Development 
+            Web Development Team
             <br/>
             <small>This is a club that hosts a huge hackathon with a straight-up aim and objective, always in the moto of bringing out the best. </small>
         </article>
       <article className='portfolio__item'>
             <h3>Shunya</h3>
-            Cotangent - Math domain.
+            Cotangent - Math Team
             <br/>
             <small>One of the most active clubs of PES with a lot of events happening now and then. Honored to be a part of this club.
             </small>
@@ -49,15 +63,21 @@ const Portfolio = () => {
         </article>
         <article className='portfolio__item'>
             <h3>Alcoding</h3>
-            Operations Team 
+            Operations & UI/UX Design Team
             <br/>
-            <small>Got into this next-level club pretty recently. I like the way they work in this club. It's all strategized and focused.</small>
+            <small>Competitive Programming based club. Kind people with great talents. Hold regular coding competitions (online) and talks (offline).</small>
         </article>
         <article className='portfolio__item'>
             <h3>VRE</h3>
             Internship Course
             <br/>
             <small>I am a huge car person and that led me to exploring this club and was part of their outstanding internship where I learnt about Onshape and Solid Works.</small>
+        </article>
+        <article className='portfolio__item'>
+            <h3>CMS - Change Maker's Society</h3>
+            UI/UX Design Team
+            <br/>
+            <small>Amazing Interview-Placement based club where they host reular meetings and interactive sessions.</small>
         </article>
       </div>
       <br />
